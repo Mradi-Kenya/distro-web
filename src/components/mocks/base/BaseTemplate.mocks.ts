3c36318c089ecbe2b2214c2d69f61728fb65330c
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-04-08 08:57:45 +0300

    build: create BaseTemplate component

---
+++ src/components/mocks/base/BaseTemplate.mocks.ts
@@ -0,0 +1,9 @@
+import { IBaseTemplate } from '../../templates/base/BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Hello world!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
